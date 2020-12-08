cb8ebc10574cb959db4b88083a56401bad5e40f2
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-12-07 19:32:03 -0500

    edits to About and Resume

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './About.css';
+import Button from 'react-bootstrap/Button';
+import resume from "../files/Leah-Haake-Resume.pdf"
 import biophoto from '../images/bio-photo.JPG';
 import {FaReact, FaNode } from 'react-icons/fa'
 import {SiJavascript, SiHtml5, SiRails, SiPostgresql, SiCss3} from 'react-icons/si'
@@ -9,14 +11,12 @@ import {DiRuby} from "react-icons/di"
 const About = () => {
     return (
         <>
-        <h1>about</h1>
+        
         <div className="about-container" id="about">
- 
-                <img src={biophoto} className="bio-image" alt="bio photo" />
-                 <h2><span className="hello">Hi! I'm Leah Haake</span></h2>   
-                <p>I'm a full stack web developer based in Maine.</p>
-                {/* <p>Graduate of General Assembly’s Software Engineering Immersive program.</p> */}
-           </div>
+             
+                <h2>Hi! I'm Leah Haake</h2>   
+                <p>Full stack web developer based in Maine</p>  
+        </div>
 
             <div className="tech"> 
                 <SiHtml5 size="30px" />
@@ -28,6 +28,16 @@ const About = () => {
                 <DiRuby size="30px" />
                 <SiRails size="40px" />
             </div>
+
+            <h1>about</h1>
+            <div className="details">
+             
+            <img src={biophoto} className="bio-image" alt="bio photo" />
+            
+             <p>I'm a motivated and detail oriented Web Developer. My background in public health drives my interest in finding technical solutions that make a positive impact. I'm a graduate of General Assembly’s Software Engineering Immersive program. <Button variant="primary"><a href={resume} target="_blank">Resume</a></Button>
+             </p>
+            </div>
+          
             
          </>
     )
