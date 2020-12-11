89ca6dbcfad9ee6aafbf2e8d7075e83c46518ba7
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-12-10 19:56:37 -0500

    Added a description to Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
     return (
         <div className="projects-header">
             <h1>projects</h1>
-       
+            <p className="projects-intro">I built the following projects from September - November 2020 while a student in General Assembly's Software Engineering Immersive program. The GitHub repos and live project links are featured below!</p>
             
             <div className="projects-container">
 
@@ -22,7 +22,7 @@ const Projects = () => {
                 <Card.Body>
                     <Card.Title>My Training Log</Card.Title>
                     <Card.Text>
-                    Capstone. Full stack web app for users to log their workouts in a digital journal, specifying: title, sport, date, time, distance, description, exertion, and more. React|Rails|Postgres|JWT
+                    Capstone. Full stack web app for users to log their workouts in a digital training log specifying: title, sport, date, time, distance, description, exertion, and more. React|Rails|Postgres|JWT
                     </Card.Text>
                     <Button variant=""><a href="https://github.com/lhaake/unit4-proj-frontend" target="_blank">Code</a></Button>
                     <Button variant=""><a href="https://lhaake-unit4proj.netlify.app/" target="_blank">Link</a></Button>
@@ -36,7 +36,7 @@ const Projects = () => {
                 <Card.Body>
                     <Card.Title>TUNR.</Card.Title>
                     <Card.Text>
-                    Full stack web app for adding songs to a playlist and select favorite songs. React|Rails|Postgres
+                    Full stack web app for adding songs to a playlist and selecting favorite songs. React|Rails|Postgres
                     </Card.Text>
                     <Button variant=""><a href="https://github.com/lhaake/tunr-rails-app-frontend" target="_blank">Code</a></Button>
                     <Button variant=""><a href="https://tunr-lhaake.netlify.app/" target="_blank">Link</a></Button>
@@ -59,8 +59,6 @@ const Projects = () => {
                 </Card.Body>
                 </Card>
                 </motion.div>
-
-
                 
                 <motion.div whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}>
                 <Card style={{ width: '18rem' }}>
@@ -69,7 +67,7 @@ const Projects = () => {
                 <Card.Body>
                     <Card.Title>Daily News</Card.Title>
                     <Card.Text>
-                    React web app using a 3rd party API that allows users to search for news articles by keyword, and select articles as favorites.
+                    React web app using a 3rd party API that allows users to search for news articles by keyword and select articles as favorites.
                     </Card.Text>
                     <Button variant=""><a href="https://lhaake-news-app.netlify.app/" target="_blank">Code</a></Button>
                     <Button variant=""><a href="https://lhaake-news-app.netlify.app/" target="_blank">Link</a></Button>
